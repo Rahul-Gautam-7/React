e2e76dfba860441685401a14a098db6d6ea36755
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-05-08 07:52:42 +0530

    set b  a normal template

--- src/App.js
+++ src/App.js
@@ -3,52 +3,71 @@ import axios from 'axios';
 import './App.css';
 import { useEffect,useState } from 'react';
 import SETC from './SETC.js';
+import SETB from './SETB';
 
 function App() {
 
-    // const [rec,setrec]=useState("");
-
-  
-
-    // axios.get('https://jsonplaceholder.typicode.com/users')
-    // .then((response)=>{
-    //     console.log(response.data)
-    // })
+    //set c
 
     const data =[
       {
         "id":1,
         "name":"Rahul Gautam",
-        "username":"rahul7"
+        "username":"rahul7",
+        "birth":"17/10/2001"
       },
 
       {
         "id":2,
         "name":"Vidhey Gadara",
-        "username":"vidhey9"
+        "username":"vidhey9",
+        "birth":"1/9/2002"
       },
 
       {
         "id":3,
         "name":"Yuvraj Tammata",
-        "username":"yuvraj18"
+        "username":"yuvraj18",
+        "birth":"23/5/2000"
       },
 
       {
         "id":3,
         "name":"Vatsal Jogi",
-        "username":"jogi11"
+        "username":"jogi11",
+        "birth":"9/7/2003"
       },
     ]
 
   return (
     <div className="App">
       
-    {
+      {/* set c */}
+
+    {/* {
       data.map(function(list){
         return <SETC name={list.name} username={list.username} id={list.id} />
       })
+    } */}
+
+
+    {/* Set B */}
+
+    {
+      data.map(function(list){
+        
+      return   <SETB name={list.name} birth={list.birth} />
+
+      })
     }
+   
+
+
+
+
+
+
+
     
     </div>
   );
--- src/SETB.js
+++ src/SETB.js
@@ -1,4 +1,8 @@
 import React,{useState,useEffect,useRef} from 'react';
+import avatar from './image/avatar.jpg';
+import phone from './image/Phone.png';
+import msg from './image/msg.jpg';
+import gmail from './image/gmail.png';
 
 function SETB(props) {
     return (
@@ -7,9 +11,18 @@ function SETB(props) {
             <div className='container-fluid'>
 
                 <div className='row'>
-                    <div className='col-4'>
+                    <div className='col-1'>
+                            <img src={avatar} className='rounded-circle border border-black mt-3' />
 
-                        
+                    </div>
+                    <div className='col-2 mt-5 mx-5 '>
+                                <h1> {props.name}</h1>
+                                <h5>{props.birth}</h5>
+                    </div>
+                    <div className='col-2 mt-5'>
+                                <img src={phone} className='rounded-circle mx-1' style={{ height:'35px' }} />
+                                <img src={msg} className='rounded-circle mx-1' style={{ height:'45px' }} />
+                                <img src={gmail} className='rounded-circle mx-1' style={{ height:'45px' }} />
 
                     </div>
                     
